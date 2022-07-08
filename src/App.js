c1f36bf437a5f092e60477c876dc8c26318a1391
ibktommy <atomquartz@gmail.com>
2022-07-08 15:53:36 +0100

    Add props to the menu component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import Category from './Category';
 import './App.css';
+import Category from './Category';
 import Menu from './Menu';
+import data from "./data";
+
 
 function App() {
   return (
@@ -9,7 +11,13 @@ function App() {
         <div className="content">
           <h1 className='title'>Menu Category</h1>
           <Category />
-          <Menu />
+          <div className="menu-content">
+            {
+              data.map(({ id, ...props }) => (
+                <Menu key={id} {...props} />
+              ))
+            }
+          </div>
         </div>
       </div>
     </>
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,27 +1,20 @@
 import React from "react";
-import image from "./item-1.jpeg";
-import data from "./data";
+// import "./App.css";
 
-const Menu = () => {
+const Menu = ({ title, category, price, img, desc }) => {
 	return (
 		<>
-			<div className="menu-content">
-				<div className="menu-item">
-					<div className="item-img">
-						<img src={image} alt="picOne" className="image" />
-					</div>
+			<div className="menu-item">
+				<div className="item-img">
+					<img src={img} alt={title} className="image" />
+				</div>
 
-					<div className="item-text">
-						<div className="top">
-							<h2 className="item-title">Buttermilk Pancakes</h2>
-							<p className="price">$15</p>
-						</div>
-						<p className="item-para">
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-							labore amet officia voluptatibus suscipit numquam quos fugiat
-							aliquam quia non.
-						</p>
+				<div className="item-text">
+					<div className="top">
+						<h2 className="item-title">{title}</h2>
+						<p className="price">{price}</p>
 					</div>
+					<p className="item-para">{desc}</p>
 				</div>
 			</div>
 		</>
