b85685193f222cbbfd4c7342c5daeec3b51967d7
ibktommy <atomquartz@gmail.com>
2022-07-08 15:34:11 +0100

    Styling the menu-items

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import image from "./item-1.jpeg";
+import data from "./data";
 
 const Menu = () => {
 	return (
 		<>
-			<div className="menu-item">
-				<div className="item-img">
-					<img src={image} alt="picOne" className="image" />
-				</div>
+			<div className="menu-content">
+				<div className="menu-item">
+					<div className="item-img">
+						<img src={image} alt="picOne" className="image" />
+					</div>
 
-				<div className="item-text">
-					<h2 className="item-title">Buttermilk Pancakes</h2>
-					<p className="item-para">
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
-						labore amet officia voluptatibus suscipit numquam quos fugiat
-						aliquam quia non.
-					</p>
+					<div className="item-text">
+						<div className="top">
+							<h2 className="item-title">Buttermilk Pancakes</h2>
+							<p className="price">$15</p>
+						</div>
+						<p className="item-para">
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint
+							labore amet officia voluptatibus suscipit numquam quos fugiat
+							aliquam quia non.
+						</p>
+					</div>
 				</div>
 			</div>
 		</>
